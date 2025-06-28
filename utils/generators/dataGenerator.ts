373f2194efe7386789bcf7a577e82262658c0bd9
Dmytro Nebelskyi <dmytro.nebelskyi@gmail.com>
2025-06-29 01:22:00 +0300

    Refactor Playwright configuration for improved readability, add UserFactory for user data generation, update IUser interface, and enhance DataGenerator with new methods for generating names and passwords.

--- utils/generators/dataGenerator.ts
+++ utils/generators/dataGenerator.ts
@@ -1,7 +1,15 @@
 import { faker } from '@faker-js/faker';
 
 export class DataGenerator {
-  static randomName(): string {
+  static randomFirstName(): string {
+    return faker.person.firstName();
+  }
+
+  static randomLastName(): string {
+    return faker.person.lastName();
+  }
+
+  static randomFullName(): string {
     return `${faker.person.firstName()} ${faker.person.lastName()}`;
   }
 
@@ -18,5 +26,9 @@ export class DataGenerator {
 
   static randomNumber(min: number = 0, max: number = 100): number {
     return faker.number.int({ min, max });
+  }
+
+  static password(length: number = 10): string {
+    return faker.string.alphanumeric({ length });
   }
 }
