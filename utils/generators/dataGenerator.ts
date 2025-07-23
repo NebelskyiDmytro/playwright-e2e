8454531ac3471d2838b08a32575f2a84c76665af
Dmytro Nebelskyi <dmytro.nebelskyi@gmail.com>
2025-07-23 15:11:35 +0300

    Refactor test and data generator methods for clarity; update test to use 'pm' parameter and rename data generation methods for consistency.

--- utils/generators/dataGenerator.ts
+++ utils/generators/dataGenerator.ts
@@ -1,19 +1,19 @@
 import { faker } from '@faker-js/faker';
 
 export class DataGenerator {
-  static randomFirstName(): string {
+  static generateRandomFirstName(): string {
     return faker.person.firstName();
   }
 
-  static randomLastName(): string {
+  static generateRandomLastName(): string {
     return faker.person.lastName();
   }
 
-  static randomFullName(): string {
+  static generateRandomFullName(): string {
     return `${faker.person.firstName()} ${faker.person.lastName()}`;
   }
 
-  static randomEmail(test?: boolean): string {
+  static generateRandomEmail(test?: boolean): string {
     if (test) {
       const username = faker.internet
         .username()
@@ -24,11 +24,11 @@ export class DataGenerator {
     return faker.internet.email();
   }
 
-  static randomNumber(min: number = 0, max: number = 100): number {
+  static generateRandomNumber(min: number = 0, max: number = 100): number {
     return faker.number.int({ min, max });
   }
 
-  static password(length: number = 10): string {
+  static generateRandomPassword(length: number = 10): string {
     return faker.string.alphanumeric({ length });
   }
 }
