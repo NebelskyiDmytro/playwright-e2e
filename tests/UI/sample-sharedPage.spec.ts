af0c984e1bec3dc5ce6fd33cd1d13ca5d5bf874e
Dmytro Nebelskyi <dmytro.nebelskyi@gmail.com>
2025-07-25 04:34:43 +0300

    Refactor login page tests to improve structure and readability by grouping related tests under a single 'Login page' test block and ensuring consistent formatting.

--- tests/UI/sample-sharedPage.spec.ts
+++ tests/UI/sample-sharedPage.spec.ts
@@ -13,28 +13,30 @@ test.afterAll(async () => {
   await teardownSharedPage();
 });
 
-test('Verify email field', async () => {
-  await expect(page.locator('#email')).toBeVisible();
-  await page.locator('#email').fill('test@test.com');
-});
+test.skip('Login page', () => {
+  test('Verify email field', async () => {
+    await expect(page.locator('#email')).toBeVisible();
+    await page.locator('#email').fill('test@test.com');
+  });
 
-test('Verify password field', async () => {
-  await expect(page.locator('#password')).toBeVisible();
-  await page.locator('#password').fill('test@test.com');
-});
+  test('Verify password field', async () => {
+    await expect(page.locator('#password')).toBeVisible();
+    await page.locator('#password').fill('test@test.com');
+  });
 
-test('Verify title', async () => {
-  const title = await page.title();
-  expect(title).toBe('Log In to Djinni'.trim());
-});
+  test('Verify title', async () => {
+    const title = await page.title();
+    expect(title).toBe('Log In to Djinni'.trim());
+  });
 
-test('Verify header text', async () => {
-  await expect(page.locator('h1')).toBeVisible();
-  await expect(page.locator('h1')).toHaveText('Log In to Djinni');
-});
+  test('Verify header text', async () => {
+    await expect(page.locator('h1')).toBeVisible();
+    await expect(page.locator('h1')).toHaveText('Log In to Djinni');
+  });
 
-socials.forEach((social) => {
-  test(`Verify login '${social}' button is visible`, async () => {
-    await expect(page.locator(`//button[@type='submit' and @value='${social}']`)).toBeVisible();
+  socials.forEach((social) => {
+    test(`Verify login '${social}' button is visible`, async () => {
+      await expect(page.locator(`//button[@type='submit' and @value='${social}']`)).toBeVisible();
+    });
   });
 });
