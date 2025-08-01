0c03a88708a7bcf29efa149f83158599d24fc160
Dmytro Nebelskyi <dmytro.nebelskyi@gmail.com>
2025-08-01 21:06:19 +0300

    Update step decorator to improve test step naming format by adding brackets around the class name.

--- decorators/step-decorator.ts
+++ decorators/step-decorator.ts
@@ -3,7 +3,7 @@ import { test } from '@playwright/test';
 function step(stepName?: string) {
   return function decorator(target: Function, context: ClassMethodDecoratorContext) {
     return function replacementMethod(this: any, ...args: any) {
-      const name = stepName || `${this.constructor.name} - ${context.name as string}`;
+      const name = stepName || `[${this.constructor.name}] - ${context.name as string}`;
       return test.step(name, async () => {
         return await target.call(this, ...args);
       });
